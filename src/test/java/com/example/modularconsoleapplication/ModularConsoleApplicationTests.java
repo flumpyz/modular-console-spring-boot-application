06bd9a0b7b2f2c874790e4fa68f88dbef1574f0c
flumpyz <natalianata2001@mail.ru>
2021-11-06 18:26:00 +0500

    initial project

---
+++ src/test/java/com/example/modularconsoleapplication/ModularConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modularconsoleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
